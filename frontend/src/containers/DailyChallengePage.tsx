28e4d658d12b2f4f5d168a5df86cb517d43c07a9
loganCurd <logan.curd@outlook.com>
2023-03-18 16:47:01 -0500

    Create daily challenge cards

--- frontend/src/containers/DailyChallengePage.tsx
+++ frontend/src/containers/DailyChallengePage.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { Button } from 'react-bootstrap'
 
 export const DailyChallengePage = () => {
     return (
         <div>
-            <h1>This is the daily challenge page</h1>
+            <br />
+            <br />
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-xl">
+                        <div className="card text-center h-100 w-100">
+                            <div className="card-header">
+                                DAILY CHALLENGE
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">Game Mode A</h5>
+                                <p className="card-text">Complete todays challenge to earn a place on the leaderboard!</p>
+                                <a href="#" className="btn btn-success">Play</a>
+                            </div>
+                            <div className="card-footer text-muted">
+                                03/18/2023
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl">
+                        <div className="card text-left h-100 w-100">
+                            <div className="card-header">
+                                Leaderboard
+                            </div>
+                            <div className="card-body text-left">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
